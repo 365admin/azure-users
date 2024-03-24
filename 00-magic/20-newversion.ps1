0684cb77110413b6c5b5949718990e6df31a2fde
Niels Gregers Johansen <niels@timeboxer.dk>
2024-03-24 16:12:40 +0100

    more

---
+++ 00-magic/20-newversion.ps1
@@ -0,0 +1,22 @@
+<#---
+title: New version
+tag: newversion
+icon: magic.png
+api: post
+---
+
+#>
+
+koksmat trace log koksmat-release bump patch .
+koksmat-release bump patch .
+
+koksmat trace log koksmat-release bump patch .
+git 
+
+
+
+koksmat trace log koksmat-release release github .
+koksmat-release release github .
+
+koksmat trace log azure-users provision webdeployproduction
+azure-users provision webdeployproduction
